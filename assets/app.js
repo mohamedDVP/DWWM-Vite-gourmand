bf8dc0606ecf1f42ac438c5b517df1100d1d14c5
mohamedDVP <mohamed.mak@laposte.net>
2026-09-12 21:31:20 +0200

    style(header): improve logo responsiveness

--- assets/app.js
+++ assets/app.js
@@ -7,4 +7,7 @@ import './stimulus_bootstrap.js';
  */
 import './styles/app.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
